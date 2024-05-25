944cb1580ac26b79777ae2c49935d0abacabfd3e
Kera Cudmore <kera.cudmore@hotmail.com>
2024-05-25 17:36:29 +0100

    remove: function that wasn't required and commented out code

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,28 +8,3 @@ function StopSound(soundobj) {
     thissound.pause();
     thissound.currentTime = 0;
 }
-
-//
-function PlaySound(soundobj) {
-    var lightSaberEffect = document.getElementById('light-saber');
-    lightSaberEffect.play();
-}
-
-function StopSound(soundobj) {
-    var lightSaberEffect = document.getElementById('light-saber');
-    lightSaberEffect.pause();
-    lightSaberEffect.currentTime = 0;
-}
-
-
-// let lightSaberEffect = document.getElementById('lightSaberEffect');
-// let homepageCard = document.querySelectorAll('homepage-card');
-
-// homepageCard.addEventListener('mouseover', function() {
-//     lightSaberEffect.play();
-// });
-
-// homepageCard.addEventListener('mouseout', function() {
-//     lightSaberEffect.pause();
-//     lightSaberEffect.currentTime = 0;
-// });
