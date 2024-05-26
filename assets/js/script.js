7a8443dfd8a30720180373ec86456907acb77e7f
2ndborn <micah.francis69@googlemail.com>
2024-05-26 08:52:54 +0000

    added dialogue mouseover dialogue to homepage cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,31 @@ function StopSound(soundobj) {
     thissound.pause();
     thissound.currentTime = 0;
 }
+
+// function reveal() {
+//     let logoText = document.getElementById('logo-text')
+//     let quizText = document.getElementById('quiz-text')
+//     let wikiText = document.getElementById('wiki-text')
+//     logoText.style.visibility = 'visible'
+//     quizText.style.visibility = 'visible'
+//     wikiText.style.visibility = 'visible'
+// }
+
+// function hideText() {
+//     let logoText = document.getElementById('logo-text')
+//     let quizText = document.getElementById('quiz-text')
+//     let wikiText = document.getElementById('wiki-text')
+//     logoText.style.visibility = 'hidden'
+//     quizText.style.visibility = 'hidden'
+//     wikiText.style.visibility = 'hidden'
+// }
+
+function reveal(textId) {
+    let text = document.getElementById(textId);
+    text.style.visibility = 'visible';
+}
+
+function hideText(textId) {
+    let text = document.getElementById(textId);
+    text.style.visibility = 'hidden';
+}
