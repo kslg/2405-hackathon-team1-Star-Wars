1ec8b03fa737a69876c8d0677334d32f661aa7f7
2ndborn <micah.francis69@googlemail.com>
2024-05-26 09:19:02 +0000

    Added text shadow to the logo-text id

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,24 +9,6 @@ function StopSound(soundobj) {
     thissound.currentTime = 0;
 }
 
-// function reveal() {
-//     let logoText = document.getElementById('logo-text')
-//     let quizText = document.getElementById('quiz-text')
-//     let wikiText = document.getElementById('wiki-text')
-//     logoText.style.visibility = 'visible'
-//     quizText.style.visibility = 'visible'
-//     wikiText.style.visibility = 'visible'
-// }
-
-// function hideText() {
-//     let logoText = document.getElementById('logo-text')
-//     let quizText = document.getElementById('quiz-text')
-//     let wikiText = document.getElementById('wiki-text')
-//     logoText.style.visibility = 'hidden'
-//     quizText.style.visibility = 'hidden'
-//     wikiText.style.visibility = 'hidden'
-// }
-
 function reveal(textId) {
     let text = document.getElementById(textId);
     text.style.visibility = 'visible';
